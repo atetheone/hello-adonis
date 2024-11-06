9885c84addd46eeec6f605d02e9a9d7178ccd7b1
atetheone <atevirran@gmail.com>
2024-11-06 16:54:48 +0000

    Add CommentsController methods for managing comments on posts

--- app/controllers/comments_controller.ts
+++ app/controllers/comments_controller.ts
@@ -1,5 +1,7 @@
 import Comment from '#models/comment'
+import Post from '#models/post'
 import type { HttpContext } from '@adonisjs/core/http'
+import { createCommentValidator } from '#validators/comment'
 
 export default class CommentsController {
   /**
@@ -21,8 +23,57 @@ export default class CommentsController {
   /**
    * Get a single comment of a post
    */
-  public async show(ctx: HttpContext) {
-    const comments = await this.index(ctx)
-    return comments.filter((comment) => comment.id === ctx.params.commentId)
+  public async show({ params }: HttpContext) {
+    const comment = await Comment.query()
+      .where('id', params.commentId)
+      .where('post_id', params.postId)
+      .firstOrFail()
+
+    return comment
+  }
+
+  /**
+   * Create a new comment for a post
+   */
+  public async store({ request, response, auth, params }: HttpContext) {
+    const data = await request.validateUsing(createCommentValidator)
+    const user = auth.user!
+    const post = await Post.firstOrFail(params.postId)
+
+    const comment = await post.related('comments').create({
+      content: data.content,
+    })
+
+    return response.created(comment)
+  }
+
+  /**
+   * Update a comment for a post
+   */
+  public async update({ request, response, params }: HttpContext) {
+    const data = await request.validateUsing(createCommentValidator)
+    const comment = await Comment.query()
+      .where('id', params.commentId)
+      .where('post_id', params.postId)
+      .firstOrFail()
+
+    comment.merge(data)
+    await comment.save()
+
+    return response.status(200).json(comment)
+  }
+
+  /**
+   * Delete a comment for a post
+   */
+  public async destroy({ response, params }: HttpContext) {
+    const comment = await Comment.query()
+      .where('id', params.commentId)
+      .where('post_id', params.postId)
+      .firstOrFail()
+
+    await comment.delete()
+
+    return response.status(204).json({ message: 'Comment deleted successfully' })
   }
 }
