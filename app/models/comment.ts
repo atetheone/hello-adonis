16077e05c4e5566d79d1d0289c778b25e937ba3a
atetheone <atevirran@gmail.com>
2024-11-06 15:13:13 +0000

    Create lucid models

---
+++ app/models/comment.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import Post from './post.js'
+import { belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+
+export default class Comment extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare content: string
+
+  @column()
+  declare userId: number
+
+  @column()
+  declare postId: number
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @belongsTo(() => Post)
+  declare post: BelongsTo<typeof Post>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
---
+++ app/models/post.ts
@@ -0,0 +1,32 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+import User from '#models/user'
+import Comment from './comment.js'
+import { belongsTo, hasMany } from '@adonisjs/lucid/orm'
+import type { BelongsTo, HasMany } from '@adonisjs/lucid/types/relations'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare content: string
+
+  @column()
+  declare userId: number
+
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  @hasMany(() => Comment)
+  declare comments: HasMany<typeof Comment>
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
