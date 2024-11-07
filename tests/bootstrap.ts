68e54e6f323dac2e2ce8c27662bb836b0c144b54
atetheone <atevirran@gmail.com>
2024-11-07 15:00:31 +0000

    Add Database import to bootstrap for testing utilities

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -4,6 +4,7 @@ import app from '@adonisjs/core/services/app'
 import type { Config } from '@japa/runner/types'
 import { pluginAdonisJS } from '@japa/plugin-adonisjs'
 import testUtils from '@adonisjs/core/services/test_utils'
+import { Database } from '@adonisjs/lucid/database'
 
 /**
  * This file is imported by the "bin/test.ts" entrypoint file
