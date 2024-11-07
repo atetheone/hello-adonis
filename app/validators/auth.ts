9ae75c3a7e820c172bca57e44c59955d9533b76f
atetheone <atevirran@gmail.com>
2024-11-07 15:00:08 +0000

    Simplify email validation in registerValidator by removing unique check

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -5,15 +5,7 @@ const password = vine.string().trim().minLength(8)
 export const registerValidator = vine.compile(
   vine.object({
     full_name: vine.string().trim().minLength(3),
-    email: vine
-      .string()
-      .trim()
-      .email()
-      .normalizeEmail()
-      .unique(async (db, value) => {
-        const match = await db.from('users').where('email', value).first()
-        return !match
-      }),
+    email: vine.string().trim().email().normalizeEmail(),
     password,
   })
 )
