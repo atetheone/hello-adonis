cc699f878458744e5e1bd7e7dd998dd1c5dd6669
atetheone <atevirran@gmail.com>
2024-11-08 16:49:00 +0000

    Enhance user registration response to include status and message

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -6,8 +6,12 @@ export default class AuthController {
   public async register({ request, response }: HttpContext) {
     const userData = await request.validateUsing(registerValidator)
     const user = await User.create(userData)
-    // const returnUser = await User.accessTokens.create(user, ['*'], { expiresIn: '1 day' })
-    return response.status(201).send(user)
+
+    return response.status(201).send({
+      status: 'success',
+      message: 'user registered successfully',
+      data: user,
+    })
   }
 
   public async login({ request, response }: HttpContext) {
