4fb0080af3d9461b79acc1c26f7f93be6a1dc794
atetheone <atevirran@gmail.com>
2024-11-08 15:47:33 +0000

    Remove redundant database check preload from adonisrc.ts

--- adonisrc.ts
+++ adonisrc.ts
@@ -43,11 +43,7 @@ export default defineConfig({
   | List of modules to import before starting the application.
   |
   */
-  preloads: [
-    () => import('#start/routes'),
-    () => import('#start/kernel'),
-    () => import('#start/check_database'),
-  ],
+  preloads: [() => import('#start/routes'), () => import('#start/kernel')],
 
   /*
   |--------------------------------------------------------------------------
