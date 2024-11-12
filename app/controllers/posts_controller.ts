f31f5ddc741eb689fa03d219e614a1051ceaf160
atetheone <atevirran@gmail.com>
2024-11-12 17:04:04 +0000

    Refactor PostsController to utilize ResponseService for consistent response handling and improve post retrieval logic

--- app/controllers/posts_controller.ts
+++ app/controllers/posts_controller.ts
@@ -1,8 +1,13 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { createPostValidator, updatePostValidator } from '#validators/post'
 import Post from '#models/post'
+import { inject } from '@adonisjs/core'
+import { ResponseService } from '#services/response_service'
+import { MESSAGES } from '#config/messages'
 
+@inject()
 export default class PostsController {
+  constructor(private responseService: ResponseService) {}
   /**
    * Display a list of posts
    */
@@ -25,20 +30,23 @@ export default class PostsController {
     // const post = await Post.create(data)
     const post = await user.related('posts').create(data)
 
-    return response.status(201).json(post)
+    // return response.status(201).json(post)
+    return this.responseService.created(response, MESSAGES.POST_CREATED, post)
   }
 
   /**
    * Show a post
    */
   async show({ response, params }: HttpContext) {
-    const post = await Post.findOrFail(params.id)
+    const post = await Post.findOrFail(params.postId)
 
     if (!post) {
-      return response.status(404).json({ message: 'Post not found' })
+      return this.responseService.error(response, MESSAGES.POST_NOT_FOUND, 404)
+      //response.status(404).json({ message: 'Post not found' })
     }
 
-    return response.status(200).json(post)
+    return this.responseService.success<Post>(response, MESSAGES.POST_SHOWED, post)
+    //.status(200).json(post)
   }
 
   /**
@@ -47,7 +55,7 @@ export default class PostsController {
   async update({ request, response, params }: HttpContext) {
     const { title, content } = await request.validateUsing(updatePostValidator)
     // check if the post exists
-    const post = await Post.findOrFail(params.id)
+    const post = await Post.findOrFail(params.postId)
 
     if (!post) {
       return response.status(404).json({ message: 'Post not found' })
@@ -65,7 +73,7 @@ export default class PostsController {
    * Delete record
    */
   async destroy({ response, params }: HttpContext) {
-    const post = await Post.findOrFail(params.id)
+    const post = await Post.findOrFail(params.postId)
     await post.delete()
     return response.status(200).json({
       message: 'Post deleted successfully',
